adea2854bde1535c484eaf971ac38d7fa2c9bc0f
CassGoss <cg159@sunyorange.edu>
2024-09-30 20:41:32 -0400

    Update c_cipher.py
    Encryption update

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -2,7 +2,12 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 def encrypt(email="abc012"):
-    """
+"""
+    Name:    Cassidy Goss
+    Date:    20240930
+    Course Number:    138
+    Class Section:    EN
+"""
     TODO: What is the objective? 
 
     Args:
@@ -19,7 +24,7 @@ def encrypt(email="abc012"):
     #     A = email[:3] (check first half)
     #     B = email[3:] (check second half)
     #     enum_flag = A or B
-    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
+   anum_flag = email[:3].isalpha() != True or email[3:].isdecimal() != True
 
     if len_flag:                         # NOTE: here we provide input validation on length
         output = "Length check failed\n"
@@ -33,14 +38,24 @@ def encrypt(email="abc012"):
         return output     
         
     # TODO: fix line below, process our string into a list
-    email_lst = ["a", "b", "c", "0", "1", "2"]
+    email_lst = list(email)
         
     # TODO: complete line(s) below, convert EACH new element into a string
     new_ascii = ord(email_lst[0]) + 3    # NOTE: here we extract and update element at 0 
     email_lst[0] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
+     new_ascii = ord(email_lst[1]) + 3    # NOTE: here we extract and update element at 1 
+    email_lst[1] = chr(new_ascii)   
+    new_ascii = ord(email_lst[2]) + 3    # NOTE: here we extract and update element at 2 
+    email_lst[2] = chr(new_ascii)    
+    new_ascii = ord(email_lst[3]) + 3    # NOTE: here we extract and update element at 3 
+    email_lst[3] = chr(new_ascii) 
+    new_ascii = ord(email_lst[4]) + 3    # NOTE: here we extract and update element at 4 
+    email_lst[4] = chr(new_ascii)    
+    new_ascii = ord(email_lst[5]) + 3    # NOTE: here we extract and update element at 5 
+    email_lst[5] = chr(new_ascii)  
         
     # TODO: fix line below, convert list into a string
-    email_str = "dbc012"
+    email_str = "".join(email_lst)
 
     # keep all updates in the retVal (str) variablei
     # i.e.,
